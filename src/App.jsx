db73a67ad87494b1bf4c076f64804f5f451a1321
Mychel Garzon <mtjomy7@gmail.com>
2023-11-30 10:30:02 +0200

    Trello #17 Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Header from './Components/Header'
+
 function App() {
   return (
     <>
-      <h1>JuDoMyKa</h1>
-      <h2>Elevate your style</h2>
+      <Header />
+
+
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,16 @@
 const Header = () => {
 
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+        <nav>
+            <ul>
+                <li><h1>JuDoMyKa</h1></li>
+                <li><h2>WOMEN</h2></li>
+                <li><h2>MEN</h2></li>
+                <li><h2>HOME</h2></li>
+                <li><h2>ABOUT</h2></li>
+                <button>LOG IN</button>
+            </ul>
+        </nav>
     )
 }
 
