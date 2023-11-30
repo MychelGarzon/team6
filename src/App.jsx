49a787378b4cbe8ed6aeae6b3224142349d53896
Jubair Hossain <jubair@Jubairs-MacBook-Pro.local>
2023-11-30 12:33:36 +0200

    trello #19 - footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './Components/Header'
 import Hero from "./Components/Hero.jsx";
+import Footer from './Components/Footer.jsx';
+
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
 
       <Header />
       <Hero />
+      <Footer />
 
     </>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+      <footer id="footer">
+
+        <p>&copy; 2023 Copyright JuDoMyKa, Inc. All rights reserved. <a href="terms">Terms</a> & <a href="privacy">Privacy</a></p>
+
+      </footer>
+    );
+  }
+
+  export default Footer;
