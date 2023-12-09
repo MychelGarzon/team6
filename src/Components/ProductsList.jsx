338b77d9e1a4e7506a1d5d24afad4cc6e2689153
Jubair Hossain <jubair@Jubairs-MBP.lan>
2023-12-09 21:07:43 +0200

    searchBar functionality added

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -3,37 +3,61 @@ import { collection, getDocs } from 'firebase/firestore';
 import { firestore } from '../firebase';
 import Card from './Card';
 
-function ProductsList() {
-    //Page title update
-    useEffect(() => {
-    
-      document.title = `Products | JUDOMYKA`;
+function ProductsList({ searchQuery }) {
+  // Page title update
+  useEffect(() => {
+    document.title = `Products | JUDOMYKA`;
+  }, []);
 
-    }, []);
+  const [products, setProducts] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [products, setProducts] = useState([]);
-  
-    useEffect(() => {
-
-
-      // Function to fetch data from Firestore
-      const fetchData = async () => {
+  useEffect(() => {
+    // Function to fetch data from Firestore
+    const fetchData = async () => {
+      try {
         const productsCollection = collection(firestore, 'products');
         const productsSnapshot = await getDocs(productsCollection);
         const productsData = productsSnapshot.docs.map((doc) => doc.data());
         setProducts(productsData);
-      };
-  
-      fetchData(); // Call the function when the component mounts
-    }, []); // Empty dependency array ensures the effect runs once after the initial render
-  
-    return (
-      <>
-        {products.map((product) => (
+      } catch (error) {
+        console.error('Error fetching products:', error);
+
+        // Check for specific error scenarios
+        if (error.code === 'permission-denied') {
+          // Handle permission denied error
+          setErrorMessage("Permission denied. Please check your access.");
+        } else if (error.code === 'resource-exhausted') {
+          // Handle resource exhausted error (e.g., database query quota exceeded)
+          setErrorMessage("Database query quota exceeded. Please try again later.");
+        } else {
+          // Handle other generic errors
+          setErrorMessage("An error occurred while fetching products. Please try again.");
+        }
+      }
+    };
+
+    fetchData(); // Call the function when the component mounts
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
+
+  // Filter products based on search query
+  const filteredProducts = products
+    ? products.filter((product) =>
+        product.title && product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : null;
+
+  return (
+    <>
+      {filteredProducts ? (
+        filteredProducts.map((product) => (
           <Card key={product.id} product={product} />
-        ))}
-      </>
-    );
-  }
-  
-  export default ProductsList;
+        ))
+      ) : (
+        <p>{errorMessage || "Loading..."}</p>
+      )}
+    </>
+  );
+}
+
+export default ProductsList;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,10 +1,27 @@
-const SearchBar = () => {
+import { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleInputChange = event => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    onSearch(query); // You can pass the query to the parent component if needed
+    };
+
+
     return (
         <div className="searchBar">
             <span className="material-symbols-outlined">
                 search
             </span>
-            <input type="text" placeholder='Search product' />
+            <input 
+            type="text" 
+            placeholder='Search products'
+            value={searchQuery}
+            onChange={handleInputChange}
+             />
         </div>
     );
 };
