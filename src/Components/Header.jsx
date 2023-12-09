9ba32495f3530e190879bb9369c8abf1e99b5745
Mychel Garzon <mtjomy7@gmail.com>
2023-12-09 13:41:18 +0200

    Trello #33
    Changes in the Header HTML, CSS, SinglePage structure and css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,30 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
-const Header = () => {
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((open) => !open);
+    };
+
     return (
-        <nav>
-            <ul>
-                <li><NavLink to="/"><h1>JuDoMyKa</h1></NavLink></li>
-                <li><NavLink to="/"><h2>HOME</h2></NavLink></li>
-                <li><NavLink to="/products"><h2>PRODUCTS</h2></NavLink></li>
-                <li><NavLink to="/about"><h2>ABOUT</h2></NavLink></li>
-                <button className="logIn_btn">LOG IN</button>
-            </ul>
-        </nav>
-    )
+        <header>
+            <div>
+                <h1 className="logo"><NavLink to="/">JuDoMyKa</NavLink></h1>
+            </div>
+            <button className="menu_trigger" onClick={toggleMenu}><span className="material-symbols-outlined"> menu </span></button>
+            <nav>
+                <ul className={`mobile ${isOpen ? "is-open" : ""}`} >
+                    <li><NavLink to="/"><h2>HOME</h2></NavLink></li>
+                    <li><NavLink to="/products"><h2>PRODUCTS</h2></NavLink></li>
+                    <li><NavLink to="/about"><h2>ABOUT</h2></NavLink></li>
+                    <li><button className="logIn_btn">LOG IN</button></li>
+                </ul>
+            </nav >
+        </header>
+    );
 }
 
-export default Header;
+
 
--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -15,20 +15,12 @@ function SinglePage() {
         <p>56.19 FREE SHIPPING</p>
         <h3>Description</h3>
         <p>Nunc mollis, erat non suscipit blandit, lacus orci ultricies lectus, ut varius nulla diam at nulla. Pellentesque eros risus, consectetur a eros sed, lacinia placerat augue. Etiam porta tincidunt lectus, quis faucibus tellus imperdiet at. Sed augue augue, porttitor a pellentesque quis, maximus ut nisi. Suspendisse posuere lorem ex, fermentum ornare ligula sodales nec. Ut viverra in mauris nec venenatis. Praesent quam leo, eleifend vitae justo ut, pulvinar tincidunt odio.</p>
+        <a onClick={() => navigate(-1)}>Back to Catalogue</a>
       </div>
 
-      <a onClick={() => navigate(-1)}>Back to Catalogue</a>
     </div >
-
 
   );
 }
 
 export default SinglePage;
-
-{/* <div className='singlePageInfo'>
-  <h2>{data.name}</h2>
-  <img src={`https://source.unsplash.com/600x600/?${urlName},${category}`} alt="picture" />
-  <p>Likes: {data.likes}</p>
-  <button onClick={() => navigate(-1)}>Return</button>
-</div> */}
